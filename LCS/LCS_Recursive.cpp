e2dcaf79ad1c67a7a3f61383be4e3ade7b3d5174
AKASH MODANWAL <akashmodanwal.fm@gmail.com>
2021-05-16 18:46:58 +0530

    Create LCS_Recursive.cpp

---
+++ LCS/LCS_Recursive.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int LCS(string x,string y,int n,int m){
+    if(n==0 || m==0)
+        return 0;
+    if(x[n-1]==y[m-1])
+        return 1+ LCS(x,y,n-1,m-1);
+    else if(x[n-1]!=y[m-1])
+        return max(LCS(x,y,n-1,m), LCS(x,y,n,m-1));
+}
+
+int main(){
+    string x= "abcdgh";
+    string y= "abedfhr";
+    cout<<"Largest Common Subsequence's Length: "<<LCS(x,y,x.length(),y.length());
+    return 0;
+}
+
